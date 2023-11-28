4a8e61a8b9de42fe951d983d25e2287c61032764
amma <2534581240@qq.com>
2023-11-28 21:16:29 +0800

    第一次提交

--- src/main/java/com/itheima/controller/EmpController.java
+++ src/main/java/com/itheima/controller/EmpController.java
@@ -1,5 +1,10 @@
 package com.itheima.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/depts")
 public class EmpController {
     //第一次提交
     //第二次提交
